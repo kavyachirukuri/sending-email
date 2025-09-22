67e8f14df537244272ce1f39532f21f9d6fcd9e3
kavyachirukuri <kavyachirukuri@users.noreply.github.com>
2025-09-22 09:21:22 +0530

    added email smtp

--- app.js
+++ app.js
@@ -13,8 +13,38 @@ app.get("/", (req, res) => {
   res.sender("home");
 });
 
+// handle email sending
 app.post("/send-email", (req, res) => {
   const { recipient, subject, message } = req.body;
 
-  nodemailer.createTransport;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "kayacelebs@gmail.com",
+      pass: "Kavya1795@",
+    },
+  });
+
+  // define the email options
+  const mailOptions = {
+    from: "kavyachirukuri@gmail.com",
+    to: recipient,
+    subject: subject,
+    text: message,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error occured:", error);
+      res.status(500).send("Error in sending email. Please try again later.");
+    } else {
+      console.log("Email sent: ", info.response);
+      res.send("Email sent successfully!");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
 });
